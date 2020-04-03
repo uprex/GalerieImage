f1d788ecc1804947415b7057ccdec717af77a9ad
Uprex <valentinhallay@gmail.com>
2020-04-03 11:05:57 +0200

    <EVO>

--- galerie/galerie/settings.py
+++ galerie/galerie/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7o%$bmmfr&#ua0)7f!@n1=3efjk)xbhi*c!4crx1$1u-u(skmg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['20.43.58.95', '127.0.0.1']
 
